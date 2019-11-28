b56d16e041ccd543456e8d9823b3a68108d0cb11
grusjose <44036914+grusjose@users.noreply.github.com>
2019-11-28 15:03:23 +0100

    Úprava normalizace dat, oprava generování datasetu

--- transform.py
+++ transform.py
@@ -14,18 +14,16 @@ def transform_data(filename, output):
     for i in range(data.shape[0]):
         if (data[i,0] == 0):
             continue
-        xs = np.zeros((1,68*2))
-        xs[::2] = 1
-        ys = np.zeros((1,68*2))
-        ys[1::2] = 1
-        maxx = np.max(t_data)
-        t_data[i, :] = data[i, :] - xs * (data[i, 60] + 0.001 * maxx * np.random.normal(loc=0, scale=3)) #posunuti do 31 bodiku a sum
-        t_data[i, :] = data[i, :] - ys * (data[i, 61] + 0.001 * maxx * np.random.normal(loc=0, scale=3))
-        a_v = np.array([t_data[i, 2]-t_data[i, 60],t_data[i, 3]-t_data[i, 61]])
-        b_v = np.array([t_data[i, 30]-t_data[i, 60],t_data[i, 31]-t_data[i, 61]])
+
+        for j in range(68):
+            t_data[i, 2*j] = -data[i, 2*j] + data[i, 60]  # posunuti do 31 bodiku
+            t_data[i, 2*j+1] = -data[i, 2*j+1] +data[i, 61]
+        a_v = np.array([t_data[i, 16]-t_data[i, 60],t_data[i, 17]-t_data[i, 61]])
+        b_v = np.array([t_data[i, 20]-t_data[i, 60],t_data[i, 21]-t_data[i, 61]])
         ration = 0.5 * (np.linalg.norm(a_v) + np.linalg.norm(b_v)) # normalizace
         #print(ration)
-        t_data[i,:] = t_data[i,:] / ration
+        t_data[i,:] = t_data[i,:] / ration + 0.001 * np.random.random(t_data[i].shape)
+        #print(t_data[i])
     np.save(output, t_data)
     print("Hotovo")
 
@@ -35,15 +33,14 @@ def create_dataset(filename, output, number_of_frames=80, gen_step=55):
     """
     data = np.load(filename).astype('f4')
     frame_num = data.shape[0]
-    print(frame_num)
     dataset = np.zeros((frame_num,number_of_frames,2*68))
     zero_mem = list()
+    print(data.shape)
     for i in range(frame_num):
-        if data[i,0] == 0:
+        if np.max(data[i]) == np.min(data[i]):
             #print("nula {}".format(i))
             zero_mem.append(i)
     zero_mem.append(frame_num)
-    print(len(zero_mem))
     last = -1
     i = 0
     while len(zero_mem) > 0:
@@ -61,17 +58,19 @@ def create_dataset(filename, output, number_of_frames=80, gen_step=55):
                 if curr + number_of_frames >= 30000:
                     pass
                     print(curr)
-                #dataset[i] = data[curr:curr+number_of_frames,:]
+                dataset[i] = data[curr:curr+number_of_frames,:]
                 i+=1
                 curr += gen_step
 
             if i > 29040:
                 pass
-                #print(curr)
             dataset[i] = data[new-number_of_frames:new,:]
             i+=1
             last = new
         #print(i)
     print("Nalezeno "+str(i))
+    for j in range(i):
+        print(np.max(dataset[j]))
     np.save(output, dataset[0:i,:,:])
     print("hotovo")
+
