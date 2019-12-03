1d84abd332556c0540d11a5dc3ee5e6eac1f6e5a
grusjose <44036914+grusjose@users.noreply.github.com>
2019-12-03 23:09:12 +0100

    Update cnn_gan.py

--- cnn_gan.py
+++ cnn_gan.py
@@ -15,7 +15,16 @@ from torch.autograd import Variable
 
 PATH = "/local/temporary/vir/hw01/pkl"
 
+"ZATÍM JSEM NETRÉNOVAL NA SERVERECH === TŘEBA OTESTOVAT A DOLADIT"
+
+
 def load_data(batch_size, filename, device, shuffle=True, store_dir='./data'):
+    """
+    data nactena jako batch velikosti batchsize reprezentuje ve formatu [B, C, F], kde B je batchsize
+    část [1, C, F], kde C je 2*68 - pocet znaků v obrázku, a F je 80 - frames ve 4 sekundách je jeden vzorek
+    load_data načítá reálná data z file input.npy
+    znaky vystupují jako channely konvoluce, blízké framy jsou spojovány konvolučním jádrem
+    """
     print(osp.join(store_dir, filename))
     p = np.load(osp.join(store_dir, filename))
     data = torch.from_numpy(p.astype('float32'))  # squeeze into correct dtype and range [0-1]
@@ -28,6 +37,10 @@ def load_data(batch_size, filename, device, shuffle=True, store_dir='./data'):
     return loader
 
 class Discriminator(torch.nn.Module):
+    """
+    zatím 1D konvoluční síť, 4 vrstvy konvoluce, na konci lineárka a sigmoida - chceme na závěr 2 psti - pokud 1, pak si síť myslí, že
+    obrázek je real, pokud 0, tak fake
+    """
     def __init__(self):
         super(Discriminator,self).__init__()
         self.conv1 = nn.Conv1d(2*68, 72, kernel_size=7)
@@ -55,6 +68,12 @@ class Discriminator(torch.nn.Module):
 
 class Generator(torch.nn.Module):
     def __init__(self):
+        """
+        zatím vezmeme noise vektor délky 100, a roztáhneme ho linárkou, pak ho sypem do konvolucni site naladene tak, aby na vysledku
+        byl pro jeden vektor 100x1 datovy format 2*68x80
+        v mnoha textech doporucuji leaky relu, ale nekde jen v diskriminatoru, trena nastudovat a upravit
+        možná bude lepší batchnorm misto instance norm - potřeba okouknout
+        """
         super(Generator, self).__init__()
         self.lin = nn.Linear(100,7200)
         self.conv1t = nn.Conv1d(20, 40, kernel_size=4,stride=2)
@@ -84,6 +103,10 @@ class Generator(torch.nn.Module):
 
 
 def sample_from_distribution(number_of_samples, dims):
+    """
+    vytváří normalizované vektory délky dims, vrátí jich to number_of_samples
+    number_of_samples jsou typicky batchsize, normální rozdělení
+    """
     samples = torch.from_numpy(np.zeros((number_of_samples, 1, dims)))
     for i in range(number_of_samples):
         sample = torch.randn(samples[0].shape)
@@ -95,48 +118,61 @@ def sample_from_distribution(number_of_samples, dims):
 def learn(discriminator, generator, opt_d, opt_g, bs, loader, epochs, device):
     discriminator.train()
     generator.train()
-    fake_label = 0
+    fake_label = 0 #faky jako 0, realy jako 1
     real_label = 1
+    last_d_loss = 5 #další hyperparaemtr k viz dále, možná pomůže, zatím nevím
     for i in range(epochs):
         print("epoch {}".format(i))
-        j = 0
-        last_d_loss = 6
-        last_g_loss = 6
         for batch, _ in loader:
             #print(batch.shape, real_labels.shape)
-            "trenink diskriminatoru"
+            #trenink diskriminatoru"
 
+            """
+            hyperparametry pro řízení iterací nad jednotlivými sítěmi
+            """
+            k_1 = 3
+            k_2 = 2
             num = batch.shape[0]
-            discriminator.zero_grad()
+            for j in range(k_1):
+                discriminator.zero_grad()
+                #vytáhni batch z dat
+                real_pred = discriminator(batch)
+                """
+                často je doporučováno zašumět labely, tedy ne čistě 1, ale třeba 0.8 - 1.2 - zkontrolovat
+                """
+                real_labels = torch.full(real_pred.shape, real_label) + (torch.rand(real_pred.shape) - 0.5) * 0.4
+                real_labels.to(device)
 
-            real_pred = discriminator(batch)
-            real_labels = torch.full(real_pred.shape, real_label) + (torch.rand(real_pred.shape) - 0.5) * 4
-            real_labels.to(device)
+                errD_real = F.binary_cross_entropy(real_pred, real_labels)
+                #bereme CL loss, chceme ji minimalizovat
+                errD_real.backward()
+                #backward, ale čekáme na fake data
+                D_x = real_pred.mean().item()
 
-            errD_real = F.binary_cross_entropy(real_pred, real_labels)
-            errD_real.backward()
-            D_x = real_pred.mean().item()
+                noise = Variable(sample_from_distribution(num, 100).float()).to(device) #sample fake dat podle batche
+                generated = generator(noise) #vygeneruj je na generatoru
+                fake_pred = discriminator(generated) #odhady diskriminatoru na fake datech
+                fake_labels = torch.full(fake_pred.shape, fake_label + 0.15) + (torch.rand(fake_pred.shape) - 0.5) * 0.3 # opět šumím labely na 0.0 - 0.3
+                fake_labels.to(device)
+                errD_fake = F.binary_cross_entropy(fake_pred, fake_labels) #fake loss
+                errD_fake.backward() #bakward a gradientnu obě dvě lossy
+                D_G_z1 = fake_pred.mean().item()
+                errD = errD_fake + errD_real
+                opt_d.step() #konečně step
+                err= errD.detach().item()
 
-            noise = Variable(sample_from_distribution(num, 100).float()).to(device)
-            generated = generator(noise)
-            fake_pred = discriminator(generated)
-            fake_labels = torch.full(fake_pred.shape, fake_label + 0.15) + (torch.rand(fake_pred.shape) - 0.5) * 3
-            fake_labels.to(device)
-            errD_fake = F.binary_cross_entropy(fake_pred, fake_labels)
-            errD_fake.backward()
-            D_G_z1 = fake_pred.mean().item()
-            errD = errD_fake + errD_real
-            opt_d.step()
-
-            opt_g.zero_grad()
-            noise = Variable(sample_from_distribution(num, 100).float()).to(device)
-            generated = generator(noise)
-            pred = discriminator(generated)
-            labels = torch.full(pred.shape, real_label)
-            labels.to(device)
-            errG = F.binary_cross_entropy(pred,labels)
-            D_G_z2 = pred.mean().item()
-            opt_g.step()
+            """hyperparametr k - ovlivnot pomoci rozdilu v ifu - kolikrat navic se trenuje generator - nechceme, aby jedna sit v loss utekla te druhe"""
+            for j in range(k_2): #trénink generátoru
+                opt_g.zero_grad()
+                noise = Variable(sample_from_distribution(num, 100).float()).to(device) #sample novýho noisu a generace nových faků
+                generated = generator(noise)
+                pred = discriminator(generated)
+                labels = torch.full(pred.shape, real_label) #jako labely beru jedničky, i když jsou to faky - kvůli lepšímu trénování generátoru
+                labels.to(device)
+                errG = F.binary_cross_entropy(pred,labels)
+                errG.backward()
+                D_G_z2 = pred.mean().item()
+                opt_g.step() #normálně backprop na generátoru
 
             if (i+1)%20==0:
                 f = str(i) + ".pt"
@@ -171,9 +207,10 @@ def main(path=""):
         generator.to(device)
         discriminator.to(device)
 
-
+    #lr zatím pro oba stejný, potřebujeme 2 optimizery, možná 2 lr?"
     lr = 0.0002
 
+    #jeden optimizer pro diskriminator a druhy pro generator"
     opt_d = optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
     opt_g = optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
     learn(discriminator, generator, opt_d, opt_g, args.batch_size, loader, args.epochs, device)
@@ -210,6 +247,7 @@ def sample_shit(generator, device='cpu'):
     return generated.detach().cpu().numpy()
 
 def visualise(sample, num):
+    "funkce pro vizualizaci, z tracker.py, možná bude třeba zakomentovat nějaké importy"
     tracker.visualise(sample, num)
 
 
@@ -217,4 +255,5 @@ if __name__ == '__main__':
     dir_path = os.path.dirname(os.path.realpath(__file__))
     print(dir_path)
     #main(dir_path)
-    sampler(1,5)
+    sampler(1,4)
+    #visualise(sample[0],12)
